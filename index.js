8bcd24647d56de03d4b4191fc79fa05e26d9789b
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-04-23 12:33:22 -0700

    Adding some JS in!

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-fetch(git@github.com:cvs181801/REST-API-Simple-Repo.git);
+const issApiUrl = "https://api.wheretheiss.at/v1/satellites/25544";
+
+const paragraph = document.getElementByID("iss-data");
+
+async function getIssData() {
+    const response = await fetch(issApiUrl);
+    const data = await response.json();
+    console.log(data);
+}
+
+getIssData();
