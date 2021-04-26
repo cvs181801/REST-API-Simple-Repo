0f7e12757499553fbbe5bce0587b4f9c431cb2b2
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-04-25 18:17:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const issApiUrl = "https://api.wheretheiss.at/v1/satellites/25544";
 async function getIssData() {
     const response = await fetch(issApiUrl);
     const data = await response.json();
-    const { latitude, longitude } = data;
+    const { latitude, longitude, altitude } = data;
 
     document.getElementById('lat').textContent = latitude;
     document.getElementById('lon').textContent = longitude;
+    document.getElementById('alti').textContent = altitude;
 }
 
 getIssData();
