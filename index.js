a645b246e7620f581518d811624dd88195506428
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-04-25 18:28:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+//API URL for the ISS data:
 const issApiUrl = "https://api.wheretheiss.at/v1/satellites/25544";
 
+//get ISS data and display latitude, longitude, and altitude:
 async function getIssData() {
     const response = await fetch(issApiUrl);
     const data = await response.json();
