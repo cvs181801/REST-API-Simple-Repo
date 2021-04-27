a7068727418c71f64c83ea630d00096f28a400e2
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-04-26 17:05:07 -0700

    adding the basic set up for leaflet map

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //API URL for the ISS data:
 const issApiUrl = "https://api.wheretheiss.at/v1/satellites/25544";
 
+//define the map (using Leaflet)
+var mymap = L.map('ISSMap').setView([51.505, -0.09], 13);
+
 //get ISS data and display latitude, longitude, and altitude:
 async function getIssData() {
     const response = await fetch(issApiUrl);
