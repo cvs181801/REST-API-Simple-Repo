7946553d123e56d82f1626a3ca167f8193f50754
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-04-27 15:58:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const issApiUrl = "https://api.wheretheiss.at/v1/satellites/25544";
 
 //define the map (using Leaflet)
-const mymap = L.map('ISSMap').setView([0,0], 1);
+const myMap = L.map('ISSMap').setView([0,0], 1);
 
 //define the map tile
 
