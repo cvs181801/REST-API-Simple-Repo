39b2f2c7ae5adb3eca25bbcd6eb53487ad64107a
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-04-27 16:07:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ const issApiUrl = "https://api.wheretheiss.at/v1/satellites/25544";
 const myMap = L.map('ISSMap').setView([0,0], 1);
 
 //define the map tile
-const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileURL,{ attribuition });
-tiles.addTo(myMap);
+//const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+//const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+//const tiles = L.tileLayer(tileURL,{ attribuition });
+//tiles.addTo(myMap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiODk4OXZhbGt5cmllODk4OSIsImEiOiJja28wbXphZm4wNzl5MnZyd2dnMG1vNDdxIn0.zR05e3yW1qQj8d0CQRZgMQ'
+}).addTo(myMap);
 
 //get ISS data and display latitude, longitude, and altitude:
 async function getIssData() {
