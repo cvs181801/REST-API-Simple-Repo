861ad2afc6b62c338c6a83de949f01f14d400a48
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-04-26 17:41:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 const issApiUrl = "https://api.wheretheiss.at/v1/satellites/25544";
 
 //define the map (using Leaflet)
-const mymap = L.map('ISSMap').setView([51.505, -0.09], 13);
+const mymap = L.map('ISSMap').setView([0,0], 13);
+
+//define the map tile
+
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileURL,{ attribuition })
+tiles.addTo(myMap);
 
 //get ISS data and display latitude, longitude, and altitude:
 async function getIssData() {
